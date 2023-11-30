5e635ede298f520b2e7f16116aa84208636a8acb
belek225 <you@example.com>
2023-11-30 23:33:13 +0700

    membuat 2.php

---
+++ 2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci nemo, autem sit ipsum molestias ab cumque est corrupti dolor voluptatem accusamus! Odit harum temporibus quidem, corrupti vitae necessitatibus facere commodi?
+        Iusto, exercitationem voluptates? Laboriosam debitis excepturi autem provident ad magni, quidem dolor mollitia, corrupti repudiandae neque, aspernatur vero reiciendis at repellat. Animi numquam ab eaque consequatur quasi soluta accusamus esse.
+        Quasi hic iusto vitae obcaecati quibusdam suscipit repellendus doloremque, sed velit, animi enim eveniet rem consequuntur aliquid, illo cumque! Exercitationem, alias. Minus, voluptates cupiditate voluptatum quos nemo consectetur aspernatur repudiandae!
+        Nemo officia laboriosam fuga commodi deserunt dignissimos reprehenderit laborum temporibus consequatur amet voluptatibus nobis iure illo molestias, in magnam vitae aliquam debitis quis accusamus nostrum alias! Atque reiciendis consequatur dolores!
+        Quasi, maxime vero nam hic corporis minima ipsam sapiente incidunt. Architecto doloribus dicta facilis libero similique ab laboriosam quis culpa odit qui aut deserunt laborum at officia, mollitia assumenda animi.
+    </h1>
+</body>
+
+</html>
