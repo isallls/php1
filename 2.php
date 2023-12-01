cf2d18c74ab42e2fe46e81625427e4ed8cc6e5e9
faisal sidauruk <ciblekrangers@gmail.com>
2023-12-01 09:27:18 +0700

    add homepage

--- 2.php
+++ 2.php
@@ -4,31 +4,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="2.css">
+    <title>Website Barang</title>
 </head>
 
 <body>
-    <marquee behavior="" direction="">
-        <h1>GACORRRRRRRRRRRRRRRRRRRRRRRRRR 599</h1>
-    </marquee>
+    <header>
+        <h1>Website Barang</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Beranda</a></li>
+                <li><a href="#">Produk</a></li>
+                <li><a href="#">Kontak</a></li>
+            </ul>
+        </nav>
+    </header>
 
-    <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci nemo, autem sit ipsum molestias ab cumque est corrupti dolor voluptatem accusamus! Odit harum temporibus quidem, corrupti vitae necessitatibus facere commodi?
-        Iusto, exercitationem voluptates? Laboriosam debitis excepturi autem provident ad magni, quidem dolor mollitia, corrupti repudiandae neque, aspernatur vero reiciendis at repellat. Animi numquam ab eaque consequatur quasi soluta accusamus esse.
-        Quasi hic iusto vitae obcaecati quibusdam suscipit repellendus doloremque, sed velit, animi enim eveniet rem consequuntur aliquid, illo cumque! Exercitationem, alias. Minus, voluptates cupiditate voluptatum quos nemo consectetur aspernatur repudiandae!
-        Nemo officia laboriosam fuga commodi deserunt dignissimos reprehenderit laborum temporibus consequatur amet voluptatibus nobis iure illo molestias, in magnam vitae aliquam debitis quis accusamus nostrum alias! Atque reiciendis consequatur dolores!
-        Quasi, maxime vero nam hic corporis minima ipsam sapiente incidunt. Architecto doloribus dicta facilis libero similique ab laboriosam quis culpa odit qui aut deserunt laborum at officia, mollitia assumenda animi.
-    </h1>
-    <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aut officia corrupti labore. Assumenda quae laborum ab vel. Accusantium rerum tenetur, enim ducimus fugiat aspernatur voluptatibus quo sint quos numquam!
-    </h1>
-    <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore mollitia distinctio recusandae deserunt ipsa laudantium itaque magni dolor! Nam accusamus consequatur consectetur cupiditate provident voluptates ipsam dolorum laboriosam fuga adipisci?
-    </h1>
-    <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem libero cum sunt aspernatur nam. Quaerat doloremque quidem rem! Laboriosam voluptatum amet minus. Culpa, laborum magni! Quia alias praesentium reprehenderit explicabo!
-    </p>
+    <section class="main-section">
+        <h2>Selamat Datang di Website Barang</h2>
+        <p>Temukan produk terbaik kami di sini!</p>
+    </section>
+
+    <section class="product-section">
+        <h2>Produk Populer</h2>
+        <div class="product">
+            <img src="product1.jpg" alt="Produk 1">
+            <h3>Produk 1</h3>
+            <p>Deskripsi produk 1...</p>
+            <button>Beli Sekarang</button>
+        </div>
+
+        <div class="product">
+            <img src="product2.jpg" alt="Produk 2">
+            <h3>Produk 2</h3>
+            <p>Deskripsi produk 2...</p>
+            <button>Beli Sekarang</button>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Website Barang. Hak Cipta Dilindungi.</p>
+    </footer>
 </body>
 
 </html>
